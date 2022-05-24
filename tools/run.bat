d1feb963d8ce40d0210ed1636d6d76b79a3d6a6d
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-05-24 14:36:47 +0300

    improvements

--- tools/run.bat
+++ tools/run.bat
@@ -1,12 +1,15 @@
 REM run.bat [source path] [source file name]
-set jdk=jdk-18
+@set jdk=jdk-18
 
-set rootdir=..
-set jdkdir=%rootdir%\jdk\%jdk%\bin
-set sourcedir=%rootdir%\%1
-set sourceName=%2
+@set rootdir=..
+@set jdkdir=%rootdir%\jdk\%jdk%\bin
+@set sourcedir=%rootdir%\%1
+@set sourceName=%2
 
-REM echo Running...
+echo Running java...
+echo.
 %jdkdir%\java.exe -cp %sourcedir%\out %sourceName%
-REM echo.
-REM pause
+echo.
+echo Java finished.
+echo.
+pause

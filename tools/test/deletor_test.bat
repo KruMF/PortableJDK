433cfb7e47ebd710641f1ccdc66bd5a333928ccb
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-05-27 18:13:23 +0300

    comments

--- tools/test/deletor_test.bat
+++ tools/test/deletor_test.bat
@@ -1,16 +1,19 @@
 @REM This script tests "deletor.bat" script.
+
 :init
 @echo off
+@REM Absolute path.
 set rootDirectory=..\..
 set testableScriptDirectory=%rootDirectory%\util
 set scriptName=deletor.bat
 
-REM relative to the testable script
+@REM Path relative to the testable script.
 set folderDirectory=test
 set folderName=deletable
 goto start
 
 
+@REM Main part of the test.
 :start
 cd "%testableScriptDirectory%"
 set folderPath=%folderDirectory%\%folderName%

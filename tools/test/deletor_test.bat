ddbf06800ac0c5f4194ccab947f182a3a2fae537
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-05-27 18:25:07 +0300

    deletor test fix

--- tools/test/deletor_test.bat
+++ tools/test/deletor_test.bat
@@ -2,13 +2,14 @@
 
 :init
 @echo off
+@title Deletor test
 @REM Absolute path.
 set rootDirectory=..\..
-set testableScriptDirectory=%rootDirectory%\util
+set testableScriptDirectory=%rootDirectory%\tools\util
 set scriptName=deletor.bat
 
 @REM Path relative to the testable script.
-set folderDirectory=test
+set folderDirectory=..\test
 set folderName=deletable
 goto start
 

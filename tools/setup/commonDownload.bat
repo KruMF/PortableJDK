5938e15b9fcefdb735be9c09605a34482ffb9793
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-05-30 21:52:48 +0300

    improvements

--- tools/setup/commonDownload.bat
+++ tools/setup/commonDownload.bat
@@ -1,11 +1,20 @@
 @REM A common usage of downloader for downloading archives.
-@REM call :prepareDownload [absolute util directory path] [url] [absolute destination path] [zip name] [title status]
+@REM call commonDownload.bat [absolute util directory path] [url] [absolute destination path] [zip name] [title status]
 @REM Path variables accept spaces.
 @REM Url variable doesn't accept spaces.
 @REM Parameter "zip name" doesn't accept spaces.
 @REM Parameter "title status" accepts spaces.
-call setupTitleChanger.bat %1 %5
-call %1\downloader.bat %2 %3 %4 %5
+:init
+set utilDir=%1
+set url=%2
+set targetDir=%3
+set zipName=%4
+set titleStatus=%5
+goto start
+
+:start
+call setupTitleChanger.bat %utilDir% %titleStatus%
+call %utilDir%\downloader.bat %url% %targetDir% %zipName% %titleStatus%
 cls
-call setupTitleChanger.bat %1 0
+call setupTitleChanger.bat %utilDir% 0
 goto :eof
--- tools/setup/commonExtraction.bat
+++ tools/setup/commonExtraction.bat
@@ -1,9 +1,17 @@
 @REM A common usage of extractor for extracting archives.
-@REM call :commonExtraction [absolute util directory path] [absolute target directory path] [zip name] [title status]
+@REM call commonExtraction.bat [absolute util directory path] [absolute target directory path] [zip name] [title status]
 @REM Path variables accept spaces.
 @REM Parameter "zip name" doesn't accept spaces.
 @REM Parameter "title status" accepts spaces.
-call setupTitleChanger.bat %1 %4
-call %1\extractor.bat %2 %3
-call setupTitleChanger.bat %1 0
+:init
+set utilDir=%1
+set targetDir=%2
+set zipName=%3
+set titleStatus=%4
+goto start
+
+:start
+call setupTitleChanger.bat %utilDir% %titleStatus%
+call %utilDir%\extractor.bat %targetDir% %zipName%
+call setupTitleChanger.bat %utilDir% 0
 goto :eof

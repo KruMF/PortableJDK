74918a381597480e0506e0d2d4e65bc88a3521c7
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-05-30 23:32:39 +0300

    improvements

--- tools/setup.bat
+++ tools/setup.bat
@@ -1,6 +1,11 @@
 @REM call setup.bat
 @REM No arguments needed nor supported.
+:init
 @echo off
+set driveLetter=%~d0
+set currentPath=%~p0
+set fullCurrentPath=%driveLetter%%currentPath%
+set fullRootPath=%fullCurrentPath%..
 cd setup
-call main.bat
+call main.bat "%fullRootPath%"
 exit

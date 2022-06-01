c58a325dc1944ba376da62c8ea0bef93f45801f1
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-06-01 22:23:12 +0300

    use of pathGetter

--- tools/setup.bat
+++ tools/setup.bat
@@ -2,10 +2,6 @@
 @REM No arguments needed nor supported.
 :init
 @echo off
-set driveLetter=%~d0
-set currentPath=%~p0
-set fullCurrentPath=%driveLetter%%currentPath%
-set fullRootPath=%fullCurrentPath%..
 cd setup
-call main.bat "%fullRootPath%"
+call main.bat
 exit
--- tools/setup/main.bat
+++ tools/setup/main.bat
@@ -1,37 +1,30 @@
-@REM call main.bat
-@REM No arguments needed nor supported.
-:start
-call :initializeConstants
-call :setConfig
-goto main
+@REM call main.bat [full root path]
+@REM Root path accepts spaces.
+:init
+set fullRootPathQuoted=
+call setupPathGetter.bat fullRootPathQuoted
+set fullRootPath=%fullRootPathQuoted:"=%
 
-
-@REM Initializes constants.
-@REM call :initializeConstants
-:initializeConstants
-set driveLetter=%~d0
-set currentPath=%~p0
-set fullCurrentPath=%driveLetter%%currentPath%
-set rootDir=..
-set utilDir=%fullCurrentPath%util
-
+set toolsDir=%fullRootPath%tools
+set utilDir=%toolsDir%\util
 set title_base=Setup
 call setupTitleChanger.bat "%utilDir%" 0
-goto :eof
+call :setConfig
+goto start
 
 
 @REM Initializes configuration.
 @REM call :setConfig
 :setConfig
 set jdkZip=jdk-zip
-set jdkDir=%fullCurrentPath%%rootDir%\jdk
+set jdkDir=%fullRootPath%\jdk
 set mavenZip=apache-maven-zip
-set mavenDir=%fullCurrentPath%%rootDir%\maven
+set mavenDir=%fullRootPath%\maven
 goto :eof
 
 
 @REM Main part of the script.
-:main
+:start
 cls
 REM read config
 REM check if setup has already been done
